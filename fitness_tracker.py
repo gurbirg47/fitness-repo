b5bfe9a4d7bf346d0f1699e8051cb70c352f79fd
gurbirg47 <gurbirg47@gmail.com>
2025-02-17 21:43:02 -0500

    added user_profile

--- fitness_tracker.py
+++ fitness_tracker.py
@@ -11,28 +11,53 @@ greet(username)
 
 
 def user_profile():
+    
     print(
         f'{username.title()}, can you please enter your age,'
         'weight, fitness goals to help us personalize your experience'
         'and provide the best recommendations for your journey'
     )
+      
+    profile = {
+         'Age' : None,
+         'Weight': None,
+         'Height': None,
+         'Goals' : None
+    }
 
+    # Collecting user details
     while True:
+        print(
+            f'\n{username.title()}, can you please enter your age,'
+            'weight, height, and fitness goals?'
+        )
         print('\n')
-        age = int(input('Enter your age: '))
-        weight = float(input('Enter your weight (lb): '))
-        height = (input('Enter your height (ft): '))
-        goals = input('What are your fitness goals? ')
+        profile['Age'] = int(input('Enter your age: '))
+        profile['Weight'] = float(input('Enter your weight (lb): '))
+        profile['Height'] = input('Enter your height (ft): ')
+        profile['Goals'] = input('What are your fitness goals? ')
 
-        print('/nSummary of your details: ')
-        print(f'Age: {age} years')
-        print(f'Weight: {weight} lb')
-        print(f'Height: {height} ft')
-        print(f'Goals: {goals}')
+        # profile summary
+        print('\nSummary of your Profile')
+        print(f'Age: {profile['Age']} years')
+        print(f'Weight: {profile['Weight']}')
+        print(f'Height: {profile['Height']} ft')
+        print(f'Goals: {profile['Goals']}')
 
-        repeat = input('\nWould you like to enter details again? (yes/no): ').strip().lower()
-        if repeat != 'yes':
-            print('Thank You!')
+        # if the user wants to make changes to their profile
+        edit = input('\nWould you like to make changes to your profile (yes/no): ').strip().lower()
+        if edit == 'yes':
+            field_to_edit = input('Which field would you like to edit (Age/Weight/Height/Goals)').strip().title()
+            if field_to_edit == 'Age':
+                profile['Age'] = int(input('Enter your new Age: '))
+            elif field_to_edit == 'Weight':
+                profile['Weight'] = float(input('Enter your new weight (ft): '))
+            elif field_to_edit == 'Height':
+                profile['Height'] = input('Enter new Height: ')
+            elif field_to_edit == 'Goals':
+                profile['Goals'] = input('Enter new Goals: ')
+        else:
+            print('Thank you! Your profile is complete.')
             break
 
 #functions call
